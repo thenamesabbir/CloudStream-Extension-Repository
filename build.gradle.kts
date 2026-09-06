87eb39e0ef7456e07eec360e0f88fc04416f21d0
Md Sabbir <103810163+thenamesabbir@users.noreply.github.com>
2026-09-06 22:01:51 +0600

    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtensionTask
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+    }
+}
+
+apply(plugin = "com.otaliastudios.tools.cloudstream")
+
+cloudstream {
+    // Repository configuration
+}
