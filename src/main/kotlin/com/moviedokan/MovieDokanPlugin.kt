db587f6cd00f7c045f07bda213933404ba66f25c
Md Sabbir <103810163+thenamesabbir@users.noreply.github.com>
2026-09-06 22:03:25 +0600

    Create MovieDokanPlugin.kt

---
+++ src/main/kotlin/com/moviedokan/MovieDokanPlugin.kt
@@ -0,0 +1,13 @@
+package com.moviedokan
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class MovieDokanPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Register MovieDokan Provider
+        registerMainAPI(MovieDokanProvider())
+    }
+}
